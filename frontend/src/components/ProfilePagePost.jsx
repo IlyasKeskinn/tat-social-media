d9803f24760db6cb21214ecf082a9a41038165e6
İlyas Keskin <ilyaskeskn8@gmail.com>
2024-05-21 08:46:22 +0300

    add : profile page post

---
+++ frontend/src/components/ProfilePagePost.jsx
@@ -0,0 +1,51 @@
+import { AspectRatio, GridItem, Image, Box, Flex } from "@chakra-ui/react";
+
+import { FaHeart } from "react-icons/fa";
+import { FaCommentAlt } from "react-icons/fa";
+
+const ProfilePagePost = ({ post }) => {
+  return (
+    <GridItem
+      h={{ base: "150px", md: "200px", lg: "300px" }}
+      position={"relative"}
+      cursor={"pointer"}
+    >
+      <AspectRatio ratio={16 / 9} overflow={"hidden"} w={"full"} h={"full"}>
+        <Image
+          src={post.image}
+          w={"full"}
+          h={"full"}
+          loading="lazy"
+          objectFit={"cover"}
+          objectPosition={"top"}
+        />
+      </AspectRatio>
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        w="full"
+        h="full"
+        bg="rgba(0, 0, 0, 0.5)"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        color="white"
+        opacity="0"
+        _hover={{ opacity: 1 }}
+        transition="opacity 0.3s"
+      >
+        <Flex alignItems={"center"} gap={2}>
+          <Flex alignItems={"center"} gap={2}>
+            85 <FaHeart />
+          </Flex>
+          <Flex alignItems={"center"} gap={2}>
+            12 <FaCommentAlt />
+          </Flex>
+        </Flex>
+      </Box>
+    </GridItem>
+  );
+};
+
+export default ProfilePagePost;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,30 @@
+import { Grid, VStack } from "@chakra-ui/react";
+
 import UserHeader from "../components/UserHeader";
+import ProfilePagePost from "../components/ProfilePagePost";
 
 const Profile = () => {
+  //TODO
+  const posts = [
+    { image: "./public/post_1.jpg" },
+    { image: "./public/post_2.jpg" },
+    { image: "./public/post_3.jpg" },
+    { image: "./public/post_4.jpg" },
+    { image: "./public/post_5.jpg" },
+    { image: "./public/post_6.jpg" },
+    { image: "./public/post_7.jpg" },
+    { image: "./public/post_8.jpg" },
+    { image: "./public/post_9.jpg" },
+  ];
   return (
-    <>
+    <VStack>
       <UserHeader />
-    </>
+      <Grid w={"full"} templateColumns={"repeat(3, 1fr)"} gap={1}>
+        {posts.map((post, index) => {
+          return <ProfilePagePost key={index} post={post} />;
+        })}
+      </Grid>
+    </VStack>
   );
 };
 
